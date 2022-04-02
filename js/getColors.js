06806c4c8a73b151843e49cc8af517440f525302
Anna Filatova <filatovaa@yahoo.com>
2022-04-02 15:30:08 -0700

    control flow updates

--- js/getColors.js
+++ js/getColors.js
@@ -1,7 +1,7 @@
 let prnt = document.getElementById("cta");
+let callCTA = document.getElementById("callCTA");
 function getColors() {
   let colors = assignValues();
-  console.log(colors);
   let div = document.createElement("div");
   div.classList.add("container-sm");
   div.id = "cta-container";
@@ -13,13 +13,16 @@ function getColors() {
   prnt.append(div);
 }
 function assignValues() {
-  const id = ["id0", "id1", "id2", "id3"];
-  const myColors = [];
-  for (let i = 0; i < 4; i++) {
-    let el = document.getElementById(id[i]).style.backgroundColor;
-    myColors.push(el);
+  if (!callCTA.classList.contains("open")) {
+    const id = ["id0", "id1", "id2", "id3"];
+    const myColors = [];
+    for (let i = 0; i < 4; i++) {
+      let el = document.getElementById(id[i]).style.backgroundColor;
+      myColors.push(el);
+    }
+    return myColors;
   }
-  return myColors;
+  return null;
 }
 
 export default getColors;

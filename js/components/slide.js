b487a4e243fd2d2d67f0e6f02ec869980b59b213
Anna Filatova <filatovaa@yahoo.com>
2022-04-04 11:02:21 -0700

    updates styles

--- js/components/slide.js
+++ js/components/slide.js
@@ -2,7 +2,7 @@ const slide = document.createElement("div");
 slide.id = "slider";
 const html = `<div>
     <a class="h3 nav-links" href="#random"><button class='btn btn-secondary btn-lg'>New Palette</button></a>
-    <a  class="h3 nav-links"href="#example"><button class='btn btn-secondary btn-lg'>View Template</button></a>
+    <a  class="h3 nav-links"href="#example"><button class='btn btn-secondary btn-lg'>Demo</button></a>
     <a  class="h3 nav-links"href="#cta"><button class='btn btn-secondary btn-lg'>Get Palette</button></a>
   </div>`;
 slide.innerHTML = html;

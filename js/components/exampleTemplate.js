1d3ff9286f9387a58d7591940bf2802a48000b91
Anna Filatova <filatovaa@yahoo.com>
2022-04-04 11:41:35 -0700

    final hamburger menu updat

--- js/components/exampleTemplate.js
+++ js/components/exampleTemplate.js
@@ -6,23 +6,7 @@ const html = `
   <nav class="navbar navbar-expand-lg bg">
     <a class="navbar-brand" id="brand" href="#">LOGO</a>
     <div class="container-fluid">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#"
-            >Home</a
-          >
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#"
-            >About</a
-          >
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#"
-            >Gallery</a
-          >
-        </li>
-      </ul>
+
       <div class="mobile">
         <div class="long mobile-nav"></div>
         <div class="short mobile-nav"></div>

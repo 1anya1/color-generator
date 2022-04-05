d32b6c6873576310b65af1ed46612acf50a4b36b
Anna Filatova <filatovaa@yahoo.com>
2022-04-05 00:29:42 -0700

    added locking feature

--- js/components/ctaTemplate.js
+++ js/components/ctaTemplate.js
@@ -1,8 +1,8 @@
 const ctaTemplate = document.createElement("div");
 ctaTemplate.id = "cta";
 const html = `
-    <p class='display-3'>Get Palette </p>
-    <p class='lead container-md'>HuePal allows you to generate a color palette randomly and dynamically see how the colors come together in a demo.</br> </br>
+    <p class='display-3'>Save Palette</p>
+    <p class='lead container-md'>
 
     If you love the color palette, click the button below to save the colors in RGB format. </p>
     <button type="button" class="btn btn-primary btn-lg" id="callCTA">
--- js/components/footer.js
+++ js/components/footer.js
@@ -1,24 +1,10 @@
-const root = document.getElementById("root");
+import svg from "./social.js";
+
 const footer = document.createElement("footer");
-const svg = document.createElement("div");
-const images = ["websiteLink.svg", "gitHub.svg", "linkedIn.svg"];
-const url = [
-  "https://anyacodes.com/",
-  "https://github.com/1anya1",
-  "https://www.linkedin.com/in/anna-filatova/",
-];
-svg.classList.add("links");
-for (let i = 0; i < 3; i++) {
-  const link = document.createElement("a");
-  link.href = url[i];
-  const img = document.createElement("img");
-  img.src = `../svg/${images[i]}`;
-  link.append(img);
-  svg.append(link);
-}
+
 const info = document.createElement("p");
 info.innerText = "Created by Anna Filatova";
-footer.append(svg);
+info.append(svg);
 footer.append(info);
 
 export default footer;
